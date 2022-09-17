b02d893253096ab48d928b25d9a56fa673cb7c73
thaisfmartins18@gmail.com <thaisfmartins18@gmail.com>
2022-09-17 15:00:53 +0000

    fatec_thais

--- train.py
+++ train.py
@@ -34,10 +34,29 @@ features = list(set (df.columns) - set([target, id_column]))
 # COMMAND ----------
 
 # DBTITLE 1,Split de dados
-X_train, X_test, y_drain, y_test = model_selection.train_test_split(df[features], df[target], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(df[features], df[target], test_size=0.2, random_state=42)
 
 # COMMAND ----------
 
 print("Geral: ", df[target].mean())
 print("Treino: ", y_train.mean())
 print("Teste: ", y_test.mean())
+
+# COMMAND ----------
+
+with mlflow.start_run():
+
+    mlflow.sklearn.autolog()
+    
+    model = ensemble.RandomForestClassifier(min_samples_leaf=18)
+    model.fit(X_train, y_train)
+
+    y_train_pred = model.predict(X_train)
+
+    acc_train = metrics.accuracy_score(y_train, y_train_pred)
+    print("Acuracia: ", acc_train)
+
+    y_test_pred = model.predict(X_test)
+
+    acc_test = metrics.accuracy_score(y_test, y_test_pred)
+    print("Acuracia: ", acc_test)
